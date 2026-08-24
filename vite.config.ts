c0f0cb9d7cfa5f28abd832352d831df36371b3de
Толя <canatolij@list.ru>
2026-08-24 13:09:43 +0300

    колесо фортуны

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const siteUrl = (process.env.VITE_SITE_URL ?? '').replace(/\/$/, '');
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'inject-site-url',
+      transformIndexHtml(html) {
+        return html.replaceAll('__SITE_URL__', siteUrl);
+      },
+    },
+  ],
 });
